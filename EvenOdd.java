7af60a3ac0ecb43d93e516ff55157f2383502afc
Subhangee <aradhya81702002@gmail.com>
2026-09-02 14:54:56 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,36 @@
+// Find largest of 3 numbers given by user.
+import java.util.*;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        var num = sc.nextInt();
+        if(num%2 == 0) System.out.println(num+" is "+"even.");
+        else System.out.println(num+" is "+"odd.");
+        sc.close();
+    }
+}
+
+/*
+Another way: using bitwise operator
+
+public class EvenOdd {
+    public static boolean isEven(int n) {
+        // Odd numbers end with 1 and even with 0 as the least significant digit
+        return (n & 1) == 0;
+    }
+
+    public static void main(String[] args) {
+        int num = 7;
+        if (isEven(num)) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
+
+Time Complexity: $O(1) constant time.
+Space Complexity: $O(1) constant space.
+*/
